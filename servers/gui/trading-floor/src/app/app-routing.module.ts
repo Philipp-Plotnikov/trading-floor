00ab2a326663b02df3d44767a99ca81447258f48
PlotnikovPhilipp <pplotnikov@datamola.com>
2021-07-02 14:07:32 +0500

    Add redirect routing

--- servers/gui/trading-floor/src/app/app-routing.module.ts
+++ servers/gui/trading-floor/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {RouterHelper} from '@shared/helpers/router.helper';
 import {ERouting} from '@entities/routing/routing.enums';
+import {REDIRECT_ROUTES} from '@entities/routing/routing.constants';
 
 const routes: Routes = [
   ...RouterHelper.getPageRouteConfigs(ERouting.Pages),
+    ...REDIRECT_ROUTES,
 ];
 
 @NgModule({
---
+++ servers/gui/trading-floor/src/app/entities/routing/routing.constants.ts
@@ -0,0 +1,11 @@
+import {Route, Routes} from '@angular/router';
+import {ERouting} from '@entities/routing/routing.enums';
+
+export const HOME_ROUTE = ERouting.Pages.Main;
+
+export const REDIRECT_ROUTES: Routes = [
+  {
+    path: '**',
+    redirectTo: HOME_ROUTE,
+  }
+];
