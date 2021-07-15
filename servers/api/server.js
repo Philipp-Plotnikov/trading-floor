1b4ac08ea220f6d36381c394e05094545cdc4542
PlotnikovPhilipp <pplotnikov@datamola.com>
2021-07-15 16:28:59 +0500

    Change server for vhoster

--- servers/api/server.js
+++ servers/api/server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
 
 app.get('/trading-floor/develop/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, '../gui/trading-floor/dist/trading-floor')});
 });
 
-app.use(express.static(path.join(__dirname, '../gui/trading-floor/dist/trading-floor')));
+app.use(express.static(path.join(__dirname, 'trading-floor/servers/gui/trading-floor/dist/trading-floor')));
 
-app.listen(port, () => {
-    console.log(`Server is listening at port: ${port}`);
-});
+exports.app = app;
