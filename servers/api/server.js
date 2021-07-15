3d14544da2c150d92b7747d1b1e6b2ed92c4d955
PlotnikovPhilipp <pplotnikov@datamola.com>
2021-07-15 16:42:26 +0500

    Change server for vhoster

--- servers/api/server.js
+++ servers/api/server.js
@@ -6,6 +6,6 @@ app.get('/trading-floor/develop/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, '../gui/trading-floor/dist/trading-floor')});
 });
 
-app.use(express.static(path.join(__dirname, 'trading-floor/servers/gui/trading-floor/dist/trading-floor')));
+app.use(express.static(path.join(__dirname, '../gui/trading-floor/dist/trading-floor')));
 
 exports.app = app;
