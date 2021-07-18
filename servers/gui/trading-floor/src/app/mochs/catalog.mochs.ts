ae7e74792cdfbda5d76c738baa6496a90bf73934
PlotnikovPhilipp <pplotnikov@datamola.com>
2021-07-19 00:33:37 +0500

    Make catalog item

--- servers/gui/trading-floor/src/app/mochs/catalog.mochs.ts
+++ servers/gui/trading-floor/src/app/mochs/catalog.mochs.ts
@@ -19,21 +19,195 @@ export namespace CatalogMochs {
 				price: 19000,
 				mark: 4,
 			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/71LJJrKbezL._AC_UL320_.jpg',
+				description: `
+				  SAMSUNG 50-Inch Class Crystal UHD AU8000 Series - 4K UHD HDR Smart TV
+				  with Alexa Built-in (UN50AU8000FXZA, 2021 Model)
+        `,
+				price: 19000,
+				mark: 4,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/71wYJc19PiL._AC_UL480_FMwebp_QL65_.jpg',
+				description: `
+				  TCL 32-inch 1080p Roku Smart LED TV - 32S327, 2019 Model
+				`,
+				price: 19000,
+				mark: 3,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/51GxgHSNolL._AC_UL320_.jpg',
+				description: `
+				  Bestol Original Genuine for Changhong LED40YC1700UA LED42YC2000UA GCBLTV50U-C1 LCD LED HDTV TV
+        `,
+				price: 1100,
+				mark: 3,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/61KEl+s4FES._AC_UL320_.jpg',
+				description: `
+				  ASA LED TV with Integrated HDTV JTV24DC
+				`,
+				price: 19000,
+				mark: 4,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/71LJJrKbezL._AC_UL320_.jpg',
+				description: `
+				  SAMSUNG 50-Inch Class Crystal UHD AU8000 Series - 4K UHD HDR Smart TV
+				  with Alexa Built-in (UN50AU8000FXZA, 2021 Model)
+        `,
+				price: 19000,
+				mark: 4,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/71wYJc19PiL._AC_UL480_FMwebp_QL65_.jpg',
+				description: `
+				  TCL 32-inch 1080p Roku Smart LED TV - 32S327, 2019 Model
+				`,
+				price: 19000,
+				mark: 3,
+			},
 		],
 		basics: [
 			{
-				imgUrl: '',
-				description: '',
-				price: 0,
-				mark: 0,
+				imgUrl: 'https://m.media-amazon.com/images/I/912IUzPWjjL._AC_UL320_.jpg',
+				description: `
+				  Amazon Basics Kids Fleece Tail Blanket - 26'' x 56'', Pink Mermaid
+				`,
+				price: 1100,
+				mark: 5,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/81ET5Y7uyKL._AC_UL320_.jpg',
+				description: `
+          Amazon Basics Kids Ultra-Soft Micromink Sherpa Blanket - Throw, Midnight Blue
+        `,
+				price: 1700,
+				mark: 5,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/81Fv47M5u5L._AC_UL320_.jpg',
+				description: `
+          Amazon Basics Kids Unicorns & Rainbows Patterned Throw Blanket
+          with Stuffed Animal Unicorn
+        `,
+				price: 1600,
+				mark: 5,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/81dsYE5uUdL._AC_UL320_.jpg',
+				description: `
+          Amazon Basics Kids Space Rocket Bedding Nap Set
+          with Rocket Pillow and Fleece Throw Blanket
+        `,
+				price: 1900,
+				mark: 5,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/912IUzPWjjL._AC_UL320_.jpg',
+				description: `
+				  Amazon Basics Kids Fleece Tail Blanket - 26'' x 56'', Pink Mermaid
+				`,
+				price: 1100,
+				mark: 5,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/81ET5Y7uyKL._AC_UL320_.jpg',
+				description: `
+          Amazon Basics Kids Ultra-Soft Micromink Sherpa Blanket - Throw, Midnight Blue
+        `,
+				price: 1700,
+				mark: 5,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/81Fv47M5u5L._AC_UL320_.jpg',
+				description: `
+          Amazon Basics Kids Unicorns & Rainbows Patterned Throw Blanket
+          with Stuffed Animal Unicorn
+        `,
+				price: 1600,
+				mark: 5,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/81dsYE5uUdL._AC_UL320_.jpg',
+				description: `
+          Amazon Basics Kids Space Rocket Bedding Nap Set
+          with Rocket Pillow and Fleece Throw Blanket
+        `,
+				price: 1900,
+				mark: 5,
 			},
 		],
 		oculus: [
 			{
-				imgUrl: '',
-				description: '',
-				price: 0,
-				mark: 0,
+				imgUrl: 'https://m.media-amazon.com/images/I/615YaAiA-ML._AC_UY218_.jpg',
+				description: `
+				  Oculus Quest 2 — Advanced All-In-One Virtual Reality Headset — 256 GB
+				`,
+				price: 29200,
+				mark: 5,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/71URNvzoWqL._AC_UY218_.jpg',
+				description: `
+				  Oculus Rift S PC-Powered VR Gaming Headset
+				`,
+				price: 36500,
+				mark: 4,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/71URNvzoWqL._AC_UY218_.jpg',
+				description: `
+				  Oculus Rift S PC-Powered VR Gaming Headset
+				`,
+				price: 35700,
+				mark: 5,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/61jBGbu2rkL._AC_UY218_.jpg',
+				description: `
+				  Flagship Oculus Quest 2 All-in-One Virtual Reality 64GB Gaming
+				  Headset w/ Fit Pack Bundle, High Resolution, PC VR Compatible,
+				  W/GM Controller Protection Controller Grip Accessories
+				`,
+				price: 31400,
+				mark: 5,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/615YaAiA-ML._AC_UY218_.jpg',
+				description: `
+				  Oculus Quest 2 — Advanced All-In-One Virtual Reality Headset — 256 GB
+				`,
+				price: 29200,
+				mark: 5,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/71URNvzoWqL._AC_UY218_.jpg',
+				description: `
+				  Oculus Rift S PC-Powered VR Gaming Headset
+				`,
+				price: 36500,
+				mark: 4,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/71URNvzoWqL._AC_UY218_.jpg',
+				description: `
+				  Oculus Rift S PC-Powered VR Gaming Headset
+				`,
+				price: 35700,
+				mark: 5,
+			},
+			{
+				imgUrl: 'https://m.media-amazon.com/images/I/61jBGbu2rkL._AC_UY218_.jpg',
+				description: `
+				  Flagship Oculus Quest 2 All-in-One Virtual Reality 64GB Gaming
+				  Headset w/ Fit Pack Bundle, High Resolution, PC VR Compatible,
+				  W/GM Controller Protection Controller Grip Accessories
+				`,
+				price: 31400,
+				mark: 5,
 			},
 		],
 		home: [
