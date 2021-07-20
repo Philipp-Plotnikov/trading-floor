c6417583983c6be09f4a3b7e6f860744536b9bb9
PlotnikovPhilipp <pplotnikov@datamola.com>
2021-07-20 11:50:45 +0500

    Make product card

--- servers/gui/trading-floor/src/app/shared/components/product-card/product-card.component.ts
+++ servers/gui/trading-floor/src/app/shared/components/product-card/product-card.component.ts
@@ -1,6 +1,4 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
-import {IProductCard} from '@entities/shared/components/product-card/product-card.interface';
-import {LoadingStatus} from '@entities/common/common.interfaces';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
 	selector: 'product-card-component',
@@ -8,7 +6,4 @@ import {LoadingStatus} from '@entities/common/common.interfaces';
 	styleUrls: ['./product-card.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductCardComponent {
-	@Input() public readonly productData: IProductCard.ProductData;
-	@Input() public readonly productDataLoadingStatus: LoadingStatus;
-}
+export class ProductCardComponent {}
--- servers/gui/trading-floor/src/app/shared/components/product-card/product-card.module.ts
+++ servers/gui/trading-floor/src/app/shared/components/product-card/product-card.module.ts
@@ -2,9 +2,20 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ProductCardComponent} from './product-card.component';
 import {ProductCardContainer} from '@shared/components/product-card/product-card.container';
+import {RightProductCardSideComponent} from './right-side/right-product-card-side.component';
+import {LeftProductCardSideComponent} from './left-side/left-product-card-side.component';
+import {RightProductCardSideContainer} from '@shared/components/product-card/right-side/right-product-card-side.container';
+import {LeftProductCardSideContainer} from '@shared/components/product-card/left-side/left-product-card-side.container';
 
 @NgModule({
-	declarations: [ProductCardComponent, ProductCardContainer],
+	declarations: [
+		ProductCardComponent,
+		ProductCardContainer,
+		RightProductCardSideComponent,
+		LeftProductCardSideComponent,
+		LeftProductCardSideContainer,
+		RightProductCardSideContainer,
+	],
 	imports: [CommonModule],
 	exports: [ProductCardContainer],
 })
