825488a0ca30f8350299565a36557469843fb39d
PlotnikovPhilipp <pplotnikov@datamola.com>
2021-08-02 14:53:05 +0500

    Make cart local storage

---
+++ servers/gui/trading-floor/src/app/shared/helpers/cart-list.helper.ts
@@ -0,0 +1,15 @@
+import {ICartList} from '@entities/shared/components/cart-list/cart-list.interfaces';
+import {IProductCard} from '@entities/shared/components/product-card/product-card.interface';
+import {LocalStorageHelper} from '@shared/helpers/local-storage.helper';
+import {CartItem} from '@entities/shared/components/cart-item/cart-item.classes';
+
+export class CartListHelper {
+	public static createCartStorageData(cartItemData: IProductCard.ProductData[]): ICartList.StorageData {
+		const result: ICartList.StorageData = Object.create(null);
+		cartItemData.forEach((cartItem) => {
+			const key = LocalStorageHelper.createCartItemKey(cartItem);
+			result[key] = new CartItem.Data(cartItem.title, cartItem.imgUrl, cartItem.amount);
+		});
+		return result;
+	}
+}
---
+++ servers/gui/trading-floor/src/app/shared/helpers/common.helper.ts
@@ -0,0 +1,1 @@
+export const getMapObject = <T>() => Object.create(null) as T;
